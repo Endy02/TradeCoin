9710cf92f5c8682674f0b18fd50c2fe332c7e8e9
jiovanyronaldvincent <jiovanyvincent.dev@outlook.com>
2020-02-17 20:12:03 +0100

    Add Autoloader class into TradeCoin project, Add methods autoload_register() and autoloader_call() into Autoloader class

--- www/core/Autoloader.class.php
+++ www/core/Autoloader.class.php
@@ -1,17 +1,19 @@
 <?php
 class Autoloader
 {
+    static function autoload_register() {
+        spl_autoload_register([__CLASS__, "autoloader_call"]);
+    }
+
     static function autoloader_call($class)
     {
-        $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-        if (file_exists("core/".$class.".class.php")) {
-            include "core/".$class.".class.php";
-        } elseif (file_exists("models/".$class.".model.php")) {
-            include "models/".$class.".model.php";
-        }
-    };
+            $class = str_replace("\\", '', $class);
 
-    static function autoload_register() {
-        spl_autoload_register("autoloader_call");
-    }:
+            if (file_exists("core/".$class.".class.php")) {
+                include "core/".$class.".class.php";
+            } elseif (file_exists("models/".$class.".model.php")) {
+                include "models/".$class.".model.php";
+            }
+    }
+
 }
