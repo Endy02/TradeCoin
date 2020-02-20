996cbee2b02d4c615279375770388d9e72196f05
jiovanyronaldvincent <jiovanyvincent.dev@outlook.com>
2020-02-20 21:41:30 +0100

    put the home page's code into front.tpl.php

--- www/views/templates/front.tpl.php
+++ www/views/templates/front.tpl.php
@@ -1,10 +1,108 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Template de front</title>
-</head>
-<body>
-	<h1>Le front</h1>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!-- ICONS FONTAWESOME -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+    <!-- BOOTSTRAP -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+
+    <title>TradeCoin</title>
+</head>
+
+<body>
+<nav class="navbar">
+    <div class="navbar-brand">
+        <img src="./public/img/bitcoin-logo.png" alt="logo trad coin">
+        <p class="first-word">Trade<span class="second-word">Coin</span></p>
+    </div>
+    <ul class="navbar-content-end">
+        <li class="navbar-item">
+            <span>Langues</span>
+        </li> <!-- DROPDOWN MENU -->
+        <li class="navbar-item">
+            <a class="btn btn-border-orange btn-login">Se connecter</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="link-sign-in">S'inscrire</a>
+        </li>
+    </ul>
+</nav>
+
+<header>
+    <img src="./public/img/come_fare_soldi_con_il_trading.jpg" alt="">
+    <div class="content-header">
+        <h1>IL EST TEMPS DE CHANGER DE NIVEAU !</h1>
+        <p>
+            Plus besoin d'avoir un master en trading pour créer son application de monétisation.
+        </p>
+        <p>Avec TradCoin cela est possible en quelques clics.</p>
+        <p>Faisons pleuvoir la crypto-monnaie.</p>
+        <form action="" class="d-flex jcc">
+            <button class="btn btn-orange money-button" type="submit">MONETISONS !</button>
+        </form>
+    </div>
+</header>
+
+<section class="plateform-container">
+    <div class="row">
+        <div class="col-md-8">
+            <img class="platform-device-img" src="../../public/img/platform_device.png" alt="">
+        </div>
+        <div class="col-md-4">
+            <h2>Créer votre application depuis n'importe quelle plateforme.</h2>
+            <p>Visualisez, analisez vos actions dans les meilleures conditions.</p>
+        </div>
+    </div>
+</section>
+
+<section class="crypto-container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Découvrez tout une variété de crypto-monnaies en constante évolution.</h2>
+            <p>Construisez votre portefeuille basé sur les crypto-monnaies.</p>
+            <p>Achetez et vendez vos actifs sur une plateforme de trading de crypto-monnaies que vous aurez créée !</p>
+        </div>
+        <div class="col-md-6">
+            <img class="cryptos" src="../../public/img/cryptogames.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="free-container d-flex jcc m-top-8">
+    <div class="free-content">
+        <h2>Vous êtes toujours parmi nous ? Parfait ! Combien cela vous coûtera ?</h2>
+        <h2>Rien ne sortira de votre poche pour construire cette machine de guerre.</h2>
+        <h1>Eh oui, c'est GRATUIT !</h1>
+        <p>Alors foncez ça se passe par ici <span class="btn btn-border-orange">C'est parti !</span></p>
+    </div>
+</section>
+
+<section class="reviews">
+    <!-- Carousel -->
+</section>
+
+<footer>
+
+</footer>
+
+
+<!-- BOOTSTRAP JS -->
+<!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
+<!-- WEBPACK -->
+<script src="./dist/bundle.js"></script>
+<!-- JS FILES -->
 </body>
 </html>
