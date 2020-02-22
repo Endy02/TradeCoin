067148b70f4b66839b4f295918e410ea6666334e
jiovanyronaldvincent <jiovanyvincent.dev@outlook.com>
2020-02-22 18:13:59 +0100

    rename demo folder to chart

--- www/public/js/chart/chart-pie-demo.js
+++ www/public/js/chart/chart-pie-demo.js
@@ -2,7 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
+// Pie chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
