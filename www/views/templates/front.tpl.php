16ff5d1f26b978e44777a1aa49004428af704a46
jiovanyronaldvincent <jiovanyvincent.dev@outlook.com>
2020-02-22 15:52:02 +0100

    add struture footer in home page

--- www/views/templates/front.tpl.php
+++ www/views/templates/front.tpl.php
@@ -23,7 +23,7 @@
 <body>
 <nav class="navbar">
     <div class="navbar-brand">
-        <img src="./public/img/bitcoin-logo.png" alt="logo trad coin">
+        <img src="./public/img/TradeCoin.png" alt="logo trade coin">
         <p class="first-word">Trade<span class="second-word">Coin</span></p>
     </div>
     <ul class="navbar-content-end">
@@ -128,10 +128,18 @@
 
             <div class="note">
                 <p>Note:</p>
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
             </div>
             <!--/.note-->
         </li>
@@ -152,10 +160,18 @@
 
             <div class="note">
                 <p>Note:</p>
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
             </div>
             <!--/.note-->
         </li>
@@ -178,11 +194,21 @@
 
             <div class="note">
                 <p>Note:</p>
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
-                <img src="../../public/img/étoile-jaune.png" alt="étoile">
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
+                <div class="d-flex" style="align-items: center">
+                    <i class="fa fa-star fa-lg" style="color: #ffe623"></i>
+                </div>
             </div>
             <!--/.note-->
         </li>
@@ -190,7 +216,46 @@
 </section>
 
 <footer>
+    <div class="contact-footer d-flex row">
+        <div class="information-tradeCoin col-md-4">
+            <div class="container-logo-footer d-flex">
+                <img class="logo-home" src="./public/img/logo_noir.png" alt="logo trade coin">
+                <h4>TradeCoin</h4>
+            </div>
+            <p>
+                TradeCoin, Créateur de plateformes de trading social, offre à la fois des investissements en actions et en crypto-monnaies.</p>
+        </div>
+        <!--/.information-tradecoin-->
 
+        <div class="social-network-contact col-md-4">
+            <h4>Nos réseaux</h4>
+            <i class="fa fa-linkedin-square fa-lg"></i>
+            <i class="fa fa-twitter-square fa-lg"></i>
+            <i class="fa fa-facebook-square fa-lg"></i>
+            <i class="fa fa-instagram fa-lg"></i>
+            <i class="fa fa-youtube-square fa-lg"></i>
+        </div>
+        <!--/.contact-footer-->
+
+        <div class="account-user-link col-md-4">
+            <h4>Compte utilisateur</h4>
+            <p><a>se connecter</a></p>
+            <p><a>s'inscire</a></p>
+        </div>
+        <!--/.account-user-link-->
+    </div>
+    <!--/.contact-footer-->
+
+    <hr style="border-style: double">
+
+    <div class="warning-footer">
+        <p>
+            Vous devriez demander l'avis d'un conseiller financier indépendant et agréé et vous assurer que vous avez le goût du risque, l'expérience et les connaissances pertinentes avant de décider de trader. Par conséquent, lorsque vous utilisez nos services de trading de crypto-monnaies, vous ne bénéficiez pas des protections offertes aux clients profitant de services d'investissement réglementés par la MIFID tels que l'accès au Fonds de compensation des investisseurs de Chypre (ICF)/Programme du fonds de compensation des services financiers (FSCS) et au Financial Ombudsman Service pour le règlement des différends. Trader avec TradeCoin en suivant et/ou en copiant ou répliquant les trades d'autres traders implique un haut niveau de risques, même si vous suivez ou copiez ou répliquez les actions des traders les plus performants. Cela inclut le risque de suivre/copier les décisions de traders éventuellement inexpérimentés/non professionnels et le risque associé aux traders dont l'objectif, l'intention ultime, ou encore le statut financier, peuvent différer du vôtre. Les performances passées d'un membre de la communauté de TradeCoin ne constituent par une indication fiable de ses performances futures. Les informations présentes dans la plateforme de trading social de TredeCoin sont générées par les membres de sa communauté et ne contiennent pas de conseils ou de recommandations donnés par ou au nom de TradeCoin - Votre Réseau Social d’Investissement.
+            <br/><br/>
+            Copyright © 2019-2020 TradeCoin - Plateforme d’investissement, tous droits réservés.
+        </p>
+    </div>
+    <!--/.warning-footer-->
 </footer>
 
 
