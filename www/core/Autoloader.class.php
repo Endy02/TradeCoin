635fba423a1814c9238cdfc960ea62d4b650dcf5
Jiovany.V <46725100+JiovanyVincent@users.noreply.github.com>
2020-03-18 13:11:22 +0100

    Update Autoloader.class.php

--- www/core/Autoloader.class.php
+++ www/core/Autoloader.class.php
@@ -1,26 +1,26 @@
 <?php
 namespace Core;
 
+/* use autoloader for al classes */
 class Autoloader
 {
     static function register() {
         spl_autoload_register([__CLASS__, "autoload"]);
     }
 
-    static function autoload($class)
+    static function autoloader_call($class)
     {
-	    if (strpos($class, __NAMESPACE__ . '\\') === 0){
-		    //var_dump($class);
-		    $class = str_replace(__NAMESPACE__ . '\\', '', $class);
-		    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-		
-		    if (file_exists($_SERVER['DOCUMENT_ROOT'].'/core/'.$class.".class.php")) {
-			    //var_dump($class);
-			    include $_SERVER['DOCUMENT_ROOT'].'/core/'.$class.".class.php";
-		    } elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/models/'.$class.".model.php")) {
-			    include $_SERVER['DOCUMENT_ROOT']."/models/". $class .".model.php";
-		    }
-	    }
+            /*
+                Constant DIRECTOTY-SEPARATOR allow to have the root of all system operator
+                For Windows it's '\' and for all others systems it's '/'
+             */
+            $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+
+            if (file_exists("core/".$class.".class.php")) {
+                require $_SERVER['DOCUMENT_ROOT'].'/core/'.$class.".class.php";
+            } elseif (file_exists("models/".$class.".model.php")) {
+                include $_SERVER['DOCUMENT_ROOT']."/models/". $class .".model.php";
+            }
     }
 
 }
