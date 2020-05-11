bd39bed199c3f56231a94792a84c72e8466cb414
Jayyaah <valentines.maitre@gmail.com>
2020-05-11 15:42:33 +0200

    add new function for user in actionController + add new routes : user

--- www/controllers/actionController.class.php
+++ www/controllers/actionController.class.php
@@ -43,6 +43,32 @@ class ActionController
         $myView = new View("singleCourses", "courses");
     }
 
+    //Users
+    public function allUserAction()
+    {
+        $myView = new View("allUser", "user");
+    }
+
+    public function addUserAction()
+    {
+        $myView = new View("addUser", "user");
+    }
+
+    public function userAction()
+    {
+        $myView = new View("user", "user");
+    }
+
+    public function userRoleAction()
+    {
+        $myView = new View("userRole", "user");
+    }
+
+    public function userPermissionAction()
+    {
+        $myView = new View("userPermission", "user");
+    }
+
     //Apparence
     public function apparenceThemeAction()
     {
